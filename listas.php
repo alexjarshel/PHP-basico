ab539c2c0994a8c1c9e3a163f85c6731ff64b5d5
unknown <alexbaukartjarschel@gmail.com>
2022-08-25 21:33:09 -0300

    adicinando listas em php

---
+++ listas.php
@@ -0,0 +1,11 @@
+<?php
+
+    $vetor = [21,55,66];
+
+    $primeironumero = $vetor[0];
+
+    //Count: quantidade de itens em um array 
+    
+    for ($i = 0; $i < count($vetor); $i++ ){
+        echo $vetor[$i] . PHP_EOL;
+    }
